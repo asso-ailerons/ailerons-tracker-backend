4a4dd377d0a258c6764683f630b0b07ea3ba2c5e
LouisCoutel <louiscoutel75@gmail.com>
2025-11-24 13:21:13 +0100

    feat: replace outdated Article model

--- ailerons_tracker_backend/models/article_model.py
+++ ailerons_tracker_backend/models/article_model.py
@@ -1,19 +1,21 @@
-""" Article Model """
+"""Article Model"""
+
+from sqlalchemy import Boolean, Identity, Text
+from sqlalchemy.dialects import postgresql
+from sqlalchemy.orm import Mapped
+from ailerons_tracker_backend.db import db
+from sqlalchemy.orm import mapped_column as mc
 
 
-class Article:
-    """ Model for a news article. """
+class Article(db.Model):
+    """Model for a news article."""
 
-    def __init__(self, form, image_url: str, published: bool = False, archived: bool = False):
-        self.title = form['newsTitle']
-        self.content = form["newsContent"]
-        self.image_url = image_url
-        self.published = published
-        self.archived = archived
-        self.publication_date = form["newsDate"]
-
-    def upload(self):
-        """ Insert the object as a new row in table 'individual' """
-
-        data = supabase.upsert(self, 'article')
-        return data
+    id: Mapped[int] = mc(
+        postgresql.BIGINT, Identity(start=1, always=True), primary_key=True, unique=True
+    )
+    title: Mapped[str] = mc(Text)
+    content: Mapped[str] = mc(Text)
+    image_url: Mapped[str] = mc(Text)
+    published: Mapped[bool] = mc(Boolean)
+    archived: Mapped[bool] = mc(Boolean)
+    publication_date: Mapped[str] = mc(postgresql.TIMESTAMP(timezone=True))
