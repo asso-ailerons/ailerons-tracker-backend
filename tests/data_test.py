0c328338dddd9d4d51556dcfd3aca906587b7926
LouisCoutel <louiscoutel75@gmail.com>
2025-11-24 13:25:38 +0100

    feat: test serving data to Android app (individual, article)

---
+++ tests/data_test.py
@@ -0,0 +1,55 @@
+import logging
+from pathlib import Path
+
+from flask import json, url_for
+
+
+resources = Path(__file__).parent / "resources"
+
+LOGGER = logging.getLogger(__name__)
+
+
+def test_individuals(app, client):
+    with app.app_context():
+        response = client.get(
+            url_for("data.get_individuals"),
+        )
+
+        assert response.status_code == 200
+
+        results = json.loads(response.data)
+        logging.info(results)
+
+        for entry in results:
+            for k in [
+                "id",
+                "createdAt",
+                "individualName",
+                "sex",
+                "commonName",
+                "binomialName",
+                "description",
+                "observationContext",
+            ]:
+                assert k in entry.keys()
+
+
+def test_articles(app, client):
+    with app.app_context():
+        response = client.get(
+            url_for("data.get_articles"),
+        )
+
+        assert response.status_code == 200
+
+        results = json.loads(response.data)
+        logging.info(results)
+
+        for entry in results:
+            for k in [
+                "title",
+                "content",
+                "publicationDate",
+                "imageUrl",
+            ]:
+                assert k in entry.keys()
