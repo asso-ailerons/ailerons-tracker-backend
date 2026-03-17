ff307ba8b6f16256fc93ab30dc539d87154957c4
LouisCoutel <louiscoutel75@gmail.com>
2026-03-17 11:30:11 +0100

    Revert "fix attempt: rework imports"
    This reverts commit 7ef45bdd5d3508d1b9b1c41eb40313b05e9c1339.

--- ailerons_tracker_backend/models/user_model.py
+++ ailerons_tracker_backend/models/user_model.py
@@ -3,7 +3,7 @@
 import os
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-from errors import EnvVarError
+from ailerons_tracker_backend.errors import EnvVarError
 
 admin_pword = os.getenv("ADMIN_PWD")
 
